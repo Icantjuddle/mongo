7f63ec74a40d80b486ab0299726429f6136b02fb
Sean Tao <sean.tao@10gen.com>
2018-07-27 14:16:42 -0400

    Fixed next to check bounds.

--- src/mongo/db/storage/biggie/biggie_sorted_impl.cpp
+++ src/mongo/db/storage/biggie/biggie_sorted_impl.cpp
@@ -39,11 +39,10 @@
 #include "mongo/platform/basic.h"
 #include "mongo/stdx/memory.h"
 #include "mongo/util/bufreader.h"
+#include "mongo/util/hex.h"
 #include "mongo/util/log.h"
 #include "mongo/util/mongoutils/str.h"
 #include "mongo/util/shared_buffer.h"
-
-#include "mongo/util/hex.h"
 
 #include <cstring>
 #include <iomanip>
@@ -266,40 +265,6 @@ SortedDataInterface::SortedDataInterface(const Ordering& ordering, bool isUnique
       isUnique(isUnique) {
     _prefixBSON = combineKeyAndRID(BSONObj(), RecordId::min(), ident.toString().append(1, '\0'), ordering);
     _postfixBSON = combineKeyAndRID(BSONObj(), RecordId::min(), _postfix, ordering);
-
-    // start printing stuff here
-    //
-
-    /*
-
-    std::string ident1 = "index-6--8250259195595751598";
-    std::string ident2 = "index-8--8250259195595751598";
-
-    std::string _prefix1 = std::string(ident1).append(1, '\1');
-    std::string _prefix2 = std::string(ident2).append(1, '\1');
-    std::string _postfix1 = std::string(ident1).append(1, '\2');
-    std::string _postfix2 = std::string(ident2).append(1, '\2');
-
-    std::string _prefixBSON1 = combineKeyAndRID(BSONObj(), RecordId::min(), std::string(ident1).append(1, '\0'), ordering);
-    std::string _prefixBSON2 = combineKeyAndRID(BSONObj(), RecordId::min(), std::string(ident2).append(1, '\0'), ordering);
-    std::string _postfixBSON1 = combineKeyAndRID(BSONObj(), RecordId::max(), _postfix1, ordering);
-    std::string _postfixBSON2 = combineKeyAndRID(BSONObj(), RecordId::max(), _postfix2, ordering);
-
-    std::string _prefixInsertSample1 = combineKeyAndRID(BSONObj(), RecordId(1), _prefix1,  ordering);
-    std::string _prefixInsertSample2 = combineKeyAndRID(BSONObj(), RecordId(1), _prefix2,  ordering);
-
-
-    log() << "Ident:" << ident1 << "\n\n";
-    log() << "PrefixBSON:" << toHex(_prefixBSON1.c_str(), _prefixBSON1.length()) << "\n\n";
-    log() << "Prefix use:" << toHex(_prefixInsertSample1.c_str(), _prefixInsertSample1.length()) << "\n\n";
-    log() << "postfixBSON:" << toHex(_postfixBSON1.c_str(), _postfixBSON1.length()) << "\n\n";
-
-    log() << "Ident:" << ident2 << "\n\n";
-    log() << "PrefixBSON:" << toHex(_prefixBSON2.c_str(), _prefixBSON2.length()) << "\n\n";
-    log() << "Prefix use:" << toHex(_prefixInsertSample2.c_str(), _prefixInsertSample2.length()) << "\n\n";
-    log() << "postfixBSON:" << toHex(_postfixBSON2.c_str(), _postfixBSON2.length()) << "\n\n";
-
-    */
 }
 
 Status SortedDataInterface::insert(OperationContext* opCtx,
@@ -381,7 +346,7 @@ void SortedDataInterface::fullValidate(OperationContext* opCtx,
     long long numKeys = 0;
     auto it = workingCopy->lower_bound(_prefixBSON);
     while (it != workingCopy->end() && it->first.compare(_postfixBSON) < 0) {
-        it = ++it;
+        ++it;
         numKeys++;
     }
     *numKeysOut = numKeys;
@@ -487,6 +452,10 @@ boost::optional<IndexKeyEntry> SortedDataInterface::Cursor::next(RequestedInfo p
                                (endPosValid && reverseIt->first == endPosReverse.get()->first))) ||
                 (_forward && (forwardIt == workingCopy->end() ||
                               (endPosValid && forwardIt->first == endPos.get()->first)))) {
+                atEOF = true;
+                return boost::none;
+            }
+            if ((_forward && forwardIt->first.compare(_postfixBSON) >= 0) || (!_forward && reverseIt->first.compare(_prefixBSON) <= 0)) {
                 atEOF = true;
                 return boost::none;
             }
